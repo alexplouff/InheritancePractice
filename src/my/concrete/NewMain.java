57467546221c8bf05d1313649f6dc544361438e2
Alex <Alex@Laptop.att.net>
2014-08-29 21:52:35 -0700

    Fixed Bug In Main Class Concrete Done Not Happy With It ( Casting )

--- src/my/concrete/NewMain.java
+++ src/my/concrete/NewMain.java
@@ -19,15 +19,32 @@ public class NewMain {
         
         
         Employee e1 = new Employee( "Alex" , "Plouff" , "E102" );
-        Employee e2 = new Employee( "Scott" , "Plouff" , "R201");
+        Employee e2 = new Employee( "Scott" , "Plouff" ,"R201");
+        Employee e3 = new Employee( "Ashley" , "Plouff" , "F422");
+        Employee e4 = new Employee( "Jamie" , "Plouff" , "A233");
+        Employee e5 = new Employee( "Cindy" , "Plouff" , "D322");
         
-        e1 = new SalariedEmployee( e1 , 75000 );
-        e2 = new SalariedEmployee ( e2 , 80000 );
+        e1 = new HourlyEmployee( 15 , 40 , e1 );
+        e2 = new HourlyEmployee( 20 , 40 , e2 );
         
-        e1 = new SalaryPlusCommissionEmployee( (SalariedEmployee) e1);
-        e2 = new SalaryPlusCommissionEmployee( (SalariedEmployee) e2);
+        e3 = new SalariedEmployee( e3 , 65000 );
         
-        System.out.println(e1 + "\n\n" + e2);
+        e4 = new SalariedEmployee( e4 , 75000 );
+        e5 = new SalariedEmployee( e5 , 80000 );
+        
+        e4 = new SalaryPlusCommissionEmployee( (SalariedEmployee) e4);
+        e5 = new SalaryPlusCommissionEmployee( (SalariedEmployee) e5);
+        
+        Employee [] employees = new Employee [ 5 ] ;
+        employees[0] = e1;
+        employees[1] = e2;
+        employees[2] = e3;
+        employees[3] = e4;
+        employees[4] = e5;
+        
+        for (Employee e : employees)
+            System.out.println(e+"\n");
+        
         
         
     }
